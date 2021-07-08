26ec1ff7de01ee55afad8ce20cb49aec8779167b
Gabriel Vartolomeu <gabrielvartolomeu@yahoo.com>
2021-07-08 22:27:45 +0300

    first commit

---
+++ with-typescript.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+;
+var res = add(3, 4);
+console.log(res);
---
+++ with-typescript.ts
@@ -0,0 +1,7 @@
+function add(a: number, b: number){
+    return a + b;
+};
+
+let res = add (3, 4);
+
+console.log(res);
